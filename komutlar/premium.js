dd6eddca59c905dd65f3f168822870a49ef05cc9
Glitch (glittery-soap) <none>
2019-07-20 10:50:08 +0000

    📒🏍 Checkpoint ./events/message.js:2358246/49 ./komutlar/premium.js:2358246/8364

--- komutlar/premium.js
+++ komutlar/premium.js
@@ -10,18 +10,18 @@ exports.run = async (client, message, args) => {
   var header = "Premium Nasıl Aktif Edilir? (Ücretsiz)"
   var msg = "Sunucunuzda botun kullandığınız her komutu sunucunuza puan kazandırmaktadır. Sunucunuz 50 puana ulaştığında Premium otomatik olarak aktif edilecek, sunucu sahibine özel mesaj olarak ve Sunucu seviyesini 50 puan yapan 50 puan için son komutun kullanıldığı kanala bildirilecektir."
   var n = "Sunucu puanını nereden göreceğim?"
-  var g = "`k!premium puan` yazarak görebilirsiniz."
+  var g = "`o!premium puan` yazarak görebilirsiniz."
   var k = "Premium Aktif Sunucular"
-  var l = "`k!premium liste` yazarak görebilirsiniz."
+  var l = "`o!premium liste` yazarak görebilirsiniz."
     if(db.has(`dil_${message.guild.id}`) === true) {
         var s = 'en'
         var a = client.commands.get('premium').help.enname
         var header = "How to Activate Premium? (Free)"
         var msg = "Every command you use on your server gives you points for your server. When your server reaches 50 points, Premium will be automatically activated, you will be notified as a private message to the server owner and to the channel where the last command is used for 50 points with 50 points."
         var n = "Where will I see the server score?"
-        var g = "You can see by writing `k!premium point`."
+        var g = "You can see by writing `o!premium point`."
         var k = "Premium Active Servers"
-        var l = "You can see by writing `k!premium list`."
+        var l = "You can see by writing `o!premium list`."
     }
     const dil = client[s]
     const o = a
@@ -31,7 +31,7 @@ var i = args.slice(0).join(' ');
   let str = ""
  for(var a = 0; a < client.guilds.size; a++) {
   if (db.has(`premium_${client.guilds.array()[a].id}`)) {
-      str += `${client.guilds.array()[a].name} \n`.replace("KONYA", "**KONYA**")
+      str += `${client.guilds.array()[a].name} \n`.replace("Oil", "**Oil**")
   }
 }
   
@@ -68,7 +68,7 @@ message.channel.send(embed)
   
   if (i === "aç" || i === "aktif") {
     
-    if (message.author.id !== "441169716547944448") return message.channel.send(`:x: Bunu Sadece Sahibim Kullanabilir !`);
+    if (message.author.id !== "290922519769382913") return message.channel.send(`:x: Bunu Sadece Sahibim Kullanabilir !`);
     
     db.set(`premium_${message.guild.id}`, "aktif")
     db.set(`sunucuxp_${message.guild.id}`, 50)
@@ -83,7 +83,7 @@ message.channel.send(embed)
   
   if (i === "kapat" || i === "deaktif" || i === "de-aktif") {
     
-    if (message.author.id !== "441169716547944448") return message.channel.send(`:x: Bunu Sadece Sahibim Kullanabilir !`);
+    if (message.author.id !== "290922519769382913") return message.channel.send(`:x: Bunu Sadece Sahibim Kullanabilir !`);
     
     db.delete(`premium_${message.guild.id}`)
     db.delete(`sunucuxp_${message.guild.id}`)
